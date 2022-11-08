53eb43f1f460fcddb09452bfbcfef06ce435c182
TheMulhima <mulhima003@gmail.com>
2022-11-08 09:33:41 -0500

    modify input field and give it more options
    1. make it use/accept string inputs
    2. allow changing of font size, placeholder, charlimit, and input width

--- BetterMenus/Elements/NumInputField.cs
+++ BetterMenus/Elements/NumInputField.cs
@@ -1,232 +1,238 @@
 ﻿using Modding.Menu;
 using Modding.Menu.Config;
-using Satchel;
-using Satchel.BetterMenus;
-using System;
-using System.Collections.Generic;
-using System.Reflection;
-using UnityEngine;
 using UnityEngine.UI;
-using UEInputField = UnityEngine.UI.InputField;
 
-namespace Satchel.BetterMenus.Elements
+namespace Satchel.BetterMenus.Elements;
+
+public class InputField : Element
 {
-    public class NumInputField : Element
+    private static GameObject _prefab;
+    public static GameObject prefab
     {
-        public GameObject CurrentField { get; set; }
-        public float Value { get; set; }
-        public float MinValue { get; set; }
-        public float MaxValue { get; set; }
-        public bool WholeNumbers { get; set; }
-
-        public Action<float> StoreValue { get; set; }
-        public Func<float> LoadValue { get; set; }
-
-        private Text _nameLabel;
-        public Text NameLabel
+        get
         {
-            get
+            if (_prefab == null)
             {
-                _nameLabel ??= CurrentField.Find("TextObj")?.GetComponentInChildren<Text>();
-                return _nameLabel;
-            }
-        }
+                var inputFieldPrefab = new GameObject();
+                GameObject.DontDestroyOnLoad(inputFieldPrefab);
+                var inputFieldPrefabRt = inputFieldPrefab.AddComponent<RectTransform>();
+                inputFieldPrefabRt.sizeDelta = new Vector2(1000f, 60f);
 
-        private static GameObject _prefab;
-        public static GameObject Prefab
-        {
-            get
-            {
-                if (_prefab is not null)
-                {
-                    return _prefab;
-                }
-                var prefab = new GameObject();
-                prefab.AddComponent<RectTransform>().sizeDelta = new Vector2(1000f, 0f);
-                var menuSelectable = prefab.AddComponent<MenuSelectable>();
-                var inputFieldObj = DefaultControls.CreateInputField(new DefaultControls.Resources());
-                inputFieldObj.name = "InputFieldObj";
-                inputFieldObj.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(300f, 60f);
+                var inputFieldObj = DefaultControls.CreateInputField(new UnityEngine.UI.DefaultControls.Resources());
+                inputFieldObj.transform.SetParent(inputFieldPrefab.transform, false);
+                inputFieldObj.name = "InputField";
+                inputFieldObj.GetComponent<RectTransform>().sizeDelta = new Vector2(300f, 60f); //to be set on build
                 inputFieldObj.RemoveComponent<Image>();
-                var field = inputFieldObj.GetComponentInChildren<UEInputField>();
-                field.textComponent.fontSize = 40;
+                inputFieldObj.transform.localPosition = new Vector3(375f,0f); //to be set on build
+                
+                var field = inputFieldObj.GetComponent<UnityEngine.UI.InputField>();
                 field.textComponent.font = MenuResources.TrajanRegular;
                 field.textComponent.color = Color.white;
                 field.textComponent.alignment = TextAnchor.MiddleCenter;
-                inputFieldObj.transform.SetParent(prefab.transform, true);
-                inputFieldObj.transform.position = new Vector3(inputFieldObj.transform.localPosition.x + 415f, inputFieldObj.transform.localPosition.y);
+                field.textComponent.text = "";
+                //to be set on build
+                field.textComponent.fontSize = 46;
+                field.contentType = UnityEngine.UI.InputField.ContentType.Alphanumeric;
+                field.characterLimit = 10;
+                
+                var placeHolder = inputFieldObj.Find("Placeholder").GetComponent<Text>();
+                placeHolder.font = MenuResources.TrajanRegular;
+                placeHolder.color = Color.grey;
+                placeHolder.alignment = TextAnchor.MiddleCenter;
+                placeHolder.text = "";
+                placeHolder.fontStyle = FontStyle.Normal;
+                placeHolder.fontSize = 46;
 
-                var underlineObj = new GameObject("UnderlineObj");
+                var underlineObj = new GameObject("Underline");
                 underlineObj.AddComponent<CanvasRenderer>();
-                var undLineRect = underlineObj.AddComponent<RectTransform>();
                 var undLineImg = underlineObj.AddComponent<Image>();
                 undLineImg.color = Color.white;
-                undLineRect.sizeDelta = new Vector2(125f, 1f);
-                underlineObj.transform.SetParent(prefab.transform, true);
-                underlineObj.transform.position = new Vector3(underlineObj.transform.position.x + 415f, underlineObj.transform.position.y - 25f);
-
-                // the text to be displayed next to the input field
-                var textObj = DefaultControls.CreateText(new DefaultControls.Resources());
-                var text = textObj.GetComponentInChildren<Text>();
-                textObj.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(700f, 60f);
+                underlineObj.transform.SetParent(inputFieldPrefab.transform, false);
+                underlineObj.transform.position += new Vector3(375f, -25f); //to be set on build
+                
+                var undLineRect = underlineObj.GetAddComponent<RectTransform>();
+                undLineRect.sizeDelta = new Vector2(125f, 3f); //to be set on build
+                
+                // Label object
+                var label = DefaultControls.CreateText(new DefaultControls.Resources());
+                var text = label.GetComponentInChildren<Text>();
+                label.GetComponent<RectTransform>().sizeDelta = new Vector2(700f, 60f);
                 text.color = Color.white;
                 text.fontSize = 46;
-                text.resizeTextMaxSize = HorizontalOptionStyle.VanillaStyle.LabelTextSize;
+                text.resizeTextMaxSize = 46;
                 text.font = MenuResources.TrajanBold;
                 text.alignment = TextAnchor.MiddleLeft;
-                textObj.name = "TextObj";
-                textObj.transform.localPosition = new Vector3(textObj.transform.localPosition.x - 150f, textObj.transform.localPosition.y);
-                textObj.transform.SetParent(prefab.transform, true);
+                label.name = "Label";
+                label.transform.localPosition += new Vector3(-150f, 0f);
+                label.transform.SetParent(inputFieldPrefab.transform, false);
 
-                // LeftCursor object
-                var cursorL = new GameObject("CursorLeft");
-                GameObject.DontDestroyOnLoad(cursorL);
-                cursorL.transform.SetParent(prefab.transform, false);
-                // CanvasRenderer
-                cursorL.AddComponent<CanvasRenderer>();
-                // RectTransform
-                var cursorLRt = cursorL.AddComponent<RectTransform>();
-                cursorLRt.sizeDelta = new Vector2(164f, 119f);
-                cursorLRt.pivot = new Vector2(0.5f, 0.5f);
-                cursorLRt.anchorMin = new Vector2(0f, 0.5f);
-                cursorLRt.anchorMax = new Vector2(0f, 0.5f);
-                cursorLRt.anchoredPosition = new Vector2(-65f, 0f);
-                cursorLRt.localScale = new Vector3(0.4f, 0.4f, 0.4f);
-                // Animator
-                var cursorLAnimator = cursorL.AddComponent<Animator>();
-                cursorLAnimator.runtimeAnimatorController = MenuResources.MenuCursorAnimator;
-                cursorLAnimator.updateMode = AnimatorUpdateMode.UnscaledTime;
-                cursorLAnimator.applyRootMotion = false;
-                // Image
-                cursorL.AddComponent<Image>();
-                menuSelectable.leftCursor = cursorLAnimator;
+                var menuSelectable = inputFieldPrefab.GetAddComponent<MenuSelectable>();
+                menuSelectable.rightCursor = Utils.CreateRegularMenuCursor(inputFieldPrefab, leftSide: false);
+                menuSelectable.leftCursor = Utils.CreateRegularMenuCursor(inputFieldPrefab, leftSide: true);
+                menuSelectable.cancelAction = CancelAction.CustomCancelAction;
 
-                // RightCursor object
-                var cursorR = new GameObject("CursorRight");
-                GameObject.DontDestroyOnLoad(cursorR);
-                cursorR.transform.SetParent(prefab.transform, false);
-                // CanvasRenderer
-                cursorR.AddComponent<CanvasRenderer>();
-                // RectTransform
-                var cursorRRt = cursorR.AddComponent<RectTransform>();
-                cursorRRt.sizeDelta = new Vector2(164f, 119f);
-                cursorRRt.pivot = new Vector2(0.5f, 0.5f);
-                cursorRRt.anchorMin = new Vector2(1f, 0.5f);
-                cursorRRt.anchorMax = new Vector2(1f, 0.5f);
-                cursorRRt.anchoredPosition = new Vector2(65f, 0f);
-                cursorRRt.localScale = new Vector3(-0.4f, 0.4f, 0.4f);
-                // Animator
-                var cursorRAnimator = cursorR.AddComponent<Animator>();
-                cursorRAnimator.runtimeAnimatorController = MenuResources.MenuCursorAnimator;
-                cursorRAnimator.updateMode = AnimatorUpdateMode.UnscaledTime;
-                cursorRAnimator.applyRootMotion = false;
-                // Image
-                cursorR.AddComponent<Image>();
-                menuSelectable.rightCursor = cursorRAnimator;
-
-                prefab.transform.position = new Vector3(prefab.transform.position.x, prefab.transform.position.y - 52.5f);
-                prefab.SetActive(false);
-                _prefab = UnityEngine.Object.Instantiate(prefab);
-                return _prefab;
+                inputFieldPrefab.SetActive(false);
+                _prefab = inputFieldPrefab;
             }
+            return _prefab;
         }
-        private UEInputField _field;
-        public UEInputField InputField
+    }
+    
+    public GameObject currentField;
+    private string _userInput = string.Empty;
+    public string userInput
+    {
+        get => _userInput;
+        set
         {
-            get
+            _userInput = value;
+            inputField.text = userInput;
+        }
+    }
+
+    private Text _label;
+    public Text label
+    {
+        get
+        {
+            if (_label == null)
             {
-                _field ??= CurrentField.Find("InputFieldObj")?.GetComponentInChildren<UEInputField>();
-                return _field;
+                _label = currentField.Find("Label")?.GetComponent<Text>();
             }
-        }
-        public NumInputField(
-            string name,
-            Action<float> storeValue,
-            Func<float> loadValue,
-            float minValue = 0f,
-            float maxValue = 9999f,
-            bool wholeNumbers = false,
-            string Id = "__Usename"
-            ) : base(Id, name)
-        {
-            Name = name;
-            WholeNumbers = wholeNumbers;
-            StoreValue = storeValue;
-            LoadValue = loadValue;
-            if (minValue >= 0f && minValue <= 9999f && maxValue >= minValue) MinValue = minValue;
-            if (maxValue >= 0f && maxValue <= 9999f && maxValue >= minValue) MaxValue = maxValue;
-        }
 
-        private GameObject AddField(GameObject fieldParent)
+            return _label;
+        }
+    }
+    
+    private Text _placeholderText;
+    public Text placeholderText
+    {
+        get
         {
-            CurrentField = UnityEngine.Object.Instantiate(Prefab, fieldParent.transform);
-            CurrentField.name = base.Name;
-            CurrentField.transform.parent = fieldParent.transform;
-
-            Action<string> updateOnEvent = newValue =>
+            if (_placeholderText == null)
             {
-                float newFloat = 0.0f;
-                int newInt = 0;
-
-                if (WholeNumbers)
-                {
-                    InputField.contentType = UEInputField.ContentType.IntegerNumber;
-                    InputField.characterLimit = 4;
-                    // same as float
-                    // also jank code
-                    var isInt = int.TryParse(newValue, out newInt);
-                    if (!isInt) return;
-                    newFloat = newInt;
-                }
-                else
-                {
-                    InputField.contentType = UEInputField.ContentType.DecimalNumber;
-                    InputField.characterLimit = 8;
-                    // returns from the delegate if inputted thing is not a number. means that it won't save anything if the input is bad
-                    var isNum = float.TryParse(newValue, out newFloat);
-                    if (!isNum) return;
-
-                }
-                // what is done when the input is changed
-                if (MinValue <= newFloat && newFloat <= MaxValue)
-                {
-                    Value = newFloat;
-                    StoreValue?.Invoke(newFloat);
-                }
-            };
-            var FieldEvent = new UEInputField.OnChangeEvent();
-            FieldEvent.AddListener(updateOnEvent.Invoke);
-            InputField.onValueChanged = FieldEvent;
-
-            CurrentField.SetActive(true);
-
-            return CurrentField;
-        }
-        public override GameObjectRow Create(ContentArea c, Menu Instance, bool AddToList = true)
-        {
-            _ = Name ?? throw new ArgumentNullException(nameof(Name), "Name cannot be null");
-            _ = StoreValue ?? throw new ArgumentNullException(nameof(StoreValue), "StoreValue cannot be null");
-            _ = LoadValue ?? throw new ArgumentNullException(nameof(LoadValue), "LoadValue cannot be null");
-            // 200 105
-            c.AddStaticPanel(Name + "Panel", new RelVector2(new Vector2(1000f, 60f)), out GameObject panel);
-            AddField(panel);
-
-            //no idea how you'd cancel this input honestly
-            c.NavGraph.AddNavigationNode(InputField);
-            if (AddToList)
-            {
-                Instance.MenuOrder.Add(new GameObjectRow(panel));
+                _placeholderText = currentField.Find("InputField")?.Find("Placeholder")?.GetComponent<Text>();
             }
-            gameObject = panel;
-            ((IContainer)Parent).OnBuilt += (_, _) => Update();
-            return new GameObjectRow(panel);
 
+            return _placeholderText;
         }
-        public override void Update()
+    }
+    
+    private UnityEngine.UI.InputField _inputField;
+    public UnityEngine.UI.InputField inputField
+    {
+        get
         {
-            NameLabel.text = base.Name;
-            Value = LoadValue.Invoke();
-            CurrentField.GetComponentInChildren<UEInputField>().text = Convert.ToString(Value);
+            if (_inputField == null)
+            {
+                _inputField = currentField.Find("InputField")?.GetComponent<UnityEngine.UI.InputField>();
+            }
+            return _inputField;
         }
+    }
+    
+    private RectTransform _underLine;
+    public RectTransform underLine
+    {
+        get
+        {
+            if (_underLine == null)
+            {
+                _underLine = currentField.Find("Underline").GetComponent<RectTransform>();
+            }
+            return _underLine;
+        }
+    }
+    
+    public int fontSize;
+    public int characterLimit;
+    public string placeholder;
+    public float inputBoxWidth;
+    public Action<string> storeValue;
+    public Func<string> loadValue;
+    public UnityEngine.UI.InputField.ContentType contentType = UnityEngine.UI.InputField.ContentType.Alphanumeric;
+
+    public InputField(
+        string name,
+        Action<string> _storeValue,
+        Func<string> _loadValue,
+        string _placeholder = "",
+        int _characterLimit = 10,
+        int _fontSize = 46,
+        int _inputBoxWidth = 300,
+        string Id = "__Usename"
+    ) : base(Id, name)
+    {
+        Name = name;
+        storeValue = _storeValue;
+        loadValue = _loadValue;
+        placeholder = _placeholder;
+        characterLimit = _characterLimit;
+        fontSize = _fontSize;
+        inputBoxWidth = _inputBoxWidth;
+    }
+    
+    private GameObject AddField(GameObject fieldParent)
+    {
+        currentField = UnityEngine.Object.Instantiate(prefab, fieldParent.transform);
+        currentField.name = base.Name;
+        currentField.transform.parent = fieldParent.transform;
+
+        var FieldEvent = new UnityEngine.UI.InputField.OnChangeEvent();
+        FieldEvent.AddListener(storeValue.Invoke);
+        inputField.onValueChanged = FieldEvent;
+
+        currentField.SetActive(true);
+
+        return currentField;
+    }
+
+    public override GameObjectRow Create(ContentArea c, Menu Instance, bool AddToList = true)
+    {
+        _ = Name ?? throw new ArgumentNullException(nameof(Name), "Name cannot be null");
+        _ = storeValue ?? throw new ArgumentNullException(nameof(storeValue), "StoreValue cannot be null");
+        _ = loadValue ?? throw new ArgumentNullException(nameof(loadValue), "LoadValue cannot be null");
+        _ = placeholder ?? throw new ArgumentNullException(nameof(placeholder), "PlaceHolder cannot be null");
+        
+        c.AddStaticPanel(Name + "Panel", new RelVector2(new Vector2(1000f, 60f)), out var panel);
+        AddField(panel);
+
+        //no idea how you'd cancel this input honestly
+        c.NavGraph.AddNavigationNode(inputField);
+        currentField.GetComponent<MenuSelectable>().customCancelAction = _ => Instance.CancelAction();
+        
+        if (AddToList)
+        {
+            Instance.MenuOrder.Add(new GameObjectRow(panel));
+        }
+        gameObject = panel;
+        
+        //to set value of fields
+        OnBuilt += Update;
+        
+        ((IContainer)Parent).OnBuilt += (_,_) => {
+            OnBuiltInvoke();             
+        };
+        return new GameObjectRow(panel);
+
+    }
+    
+
+    public override void Update()
+    {
+        placeholderText.text = placeholder;
+        label.text = base.Name;
+        userInput = loadValue();
+        
+        inputField.contentType = contentType;
+        inputField.textComponent.fontSize = fontSize;
+        inputField.characterLimit = characterLimit;
+
+        var inputFieldRt = inputField.GetComponent<RectTransform>();
+
+        inputFieldRt.sizeDelta = inputFieldRt.sizeDelta with { x = inputBoxWidth };
+        inputFieldRt.transform.localPosition = inputFieldRt.transform.localPosition with { x = 500 - inputBoxWidth * 0.45f };
+        underLine.sizeDelta = underLine.sizeDelta with { x = inputBoxWidth * 0.9f }; 
+        underLine.transform.localPosition = underLine.transform.localPosition with { x = 500 - inputBoxWidth * 0.45f };
     }
 }
